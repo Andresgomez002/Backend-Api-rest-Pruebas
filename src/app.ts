4e816f8301bd02f5de7a4660d306630e67613a4c
Nicolas Gomez <nicolasgomez8873030@gmail.com>
2023-06-28 20:30:24 -0500

    [+] se definieron dos endpoints, /, /home

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
 import  express  from "express";
 const app = express();
+//http://localhost:3000
+app.get('/', (req, res)=>{
+const mensaje = 'Bienvenido a la API de manga page';
+console.log(mensaje);
+    res.send(`<h1>${mensaje}</h1>`)
+})
+app.get('/home',(req, res)=>{
+    const namePage = 'Home';
+    console.log(namePage);
+    res.send(`<h1>${namePage}</h1>`)
+} )
 app.listen(3000, ()=>{
     console.log('servidor lanzado en el puerto 3000');
 });
