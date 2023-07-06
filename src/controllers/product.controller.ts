35738721563f89bf695d2085e5db8de9e73abf8c
Nicolas Gomez <nicolasgomez8873030@gmail.com>
2023-07-05 19:03:44 -0500

    [+] product,model/product.service: controlar exepcionesnen la  creacion de productos

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -8,11 +8,18 @@ async function getproduct( req: Request, res:Response){
       console.log('Obtiene un producto por id');    
       res.send('Obtiene un producto por id');
 }
-async function createProduct( req: Request, res:Response){
-      console.log(req.body);
-    const data = await insertProduct(req.body);
-    console.log(data);
-    res.json(data);
+async function createProduct( req: Request, res:Response){      
+      try{
+            const data = await insertProduct(req.body);
+            console.log(data);
+            res.json(data);
+      }catch(error){
+       console.log('Error en la insercion del producto');
+       res.json({
+            msg: 'ERROR_INSERT_PRODUCT'
+       });
+      }
+   
 }
 async function updateProduct( req: Request, res:Response){
       console.log('Actualiza un producto por id');   
